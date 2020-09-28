dbf6795155454b41c55deecee9d10e7f68195d08
Troy Leach <troyleach29@gmail.com>
2020-09-27 19:31:40 -0500

    moved lib folder into app folder for autoloader to work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # include Error::ErrorHandler
+  # FIXME:
+  # had the lib folder in the wrong place. moved lib folder into app.
+  include Error::ErrorHandler
 end
